dd269df3497a3482a5db93f009ce32f87bf9f88e
anavp <anavprasadanav@gmail.com>
2022-06-17 15:48:02 -0400

    codeforces round 800 div 2

---
+++ codeforces/codeforces_800_div2/creep.cpp
@@ -0,0 +1,131 @@
+/***************************************************
+* AUTHOR : Anav Prasad
+* Nick : vrangr
+****************************************************/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <assert.h>
+
+using namespace std;
+
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define forsn(i, st_val, n) for (int i = st_val; i <= n; ++i)
+#define forr(i, n) for (int i = n - 1; i >= 0; --i)
+#define forsr(i, st_val, n) for (int i = st_val; i >= n; --i)
+#define pb1(a) push_back(a)
+#define pb2(a,b) push_back({a, b})
+#define GET_MACRO(_1,_2,_3,NAME,...) NAME
+#define pb(...) GET_MACRO(__VA_ARGS__, pb1, pb2)(__VA_ARGS__)
+#define pass (void)0
+#define print_var(x) cout << #x << ": " << x << "\n";
+#define space " "
+typedef long long int ll;
+typedef unsigned long long int ull;
+template <typename type> void print(const vector<vector<type> > &arr);
+template <typename type> void print(const vector<type> &arr);
+template <typename type> void off_print(const vector<type> &arr);
+template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr);
+template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr);
+template <typename t1, typename t2> void print(const pair<t1, t2> &p);
+
+void solve(){
+    int a, b;
+    cin >> a >> b;
+    string ans = "";
+    while(a > 0 && b > 0){
+        ans += "10";
+        a--;
+        b--;
+    }
+    while (a > 0){
+        a--;
+        ans.push_back('0');
+    }
+    while(b > 0){
+        b--;
+        ans.push_back('1');
+    }
+    cout << ans << endl;
+}
+
+int main(){
+	fastIO;
+	TEST;
+	return 0;
+}
+
+
+template <typename type> void print(const vector<vector<type> > &arr){
+	cout << "\n[";
+	forn(i, arr.size()){
+		cout << "[";
+		forn(j, arr[i].size() - 1)
+			cout << arr[i][j] << ", ";
+		cout << arr[i][arr[i].size() - 1] << "]";
+		if (i != arr.size() - 1)
+			cout << "," << endl;
+	}
+	cout << "], 2D Vector\n";
+}
+
+
+template <typename type> void print(const vector<type> &arr){
+	cout << "\n[";
+	forn(i, arr.size()){
+		cout << arr[i];
+		if (i != arr.size() - 1)
+			cout << ", ";
+	}
+	cout << "], 1D Vector\n";
+}
+
+template <typename type> void off_print(const vector<type> &arr){
+	forn(i, arr.size())
+		cout << arr[i] << " ";
+}
+
+
+template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr){
+	int n = arr.size();
+	cout << "\n[";
+	forn(i, n - 1){
+		cout << "{" << arr[i].first << "," << arr[i].second << "}, ";
+	}
+	cout << "{" << arr[n - 1].first << "," << arr[n - 1].second << "}], 1D Vector of Pairs\n";
+}
+
+
+template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr){
+	cout << "\n[";
+	forn(i, arr.size()){
+		cout << "[";
+		forn(j, arr[i].size()){
+			cout << "{" << arr[i][j].first << "," << arr[i][j].second << "}";
+			if (j != arr[i].size() - 1)
+				cout << ", ";
+		}
+		cout << "]";
+		if (i != arr.size() - 1)
+			cout << "," << endl;
+	}
+	cout << "], 2D Vector of Pairs\n";
+}
+
+
+template <typename t1, typename t2> void print(const pair<t1, t2> &p){
+	cout << "\n{" << p.first << "," << p.second << "}, Pair\n";
+}
---
+++ codeforces/codeforces_800_div2/directional_increase.cpp
@@ -0,0 +1,162 @@
+/***************************************************
+* AUTHOR : Anav Prasad
+* Nick : vrangr
+****************************************************/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <assert.h>
+
+using namespace std;
+
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define forsn(i, st_val, n) for (int i = st_val; i <= n; ++i)
+#define forr(i, n) for (int i = n - 1; i >= 0; --i)
+#define forsr(i, st_val, n) for (int i = st_val; i >= n; --i)
+#define pb1(a) push_back(a)
+#define pb2(a,b) push_back({a, b})
+#define GET_MACRO(_1,_2,_3,NAME,...) NAME
+#define pb(...) GET_MACRO(__VA_ARGS__, pb1, pb2)(__VA_ARGS__)
+#define pass (void)0
+#define print_var(x) cout << #x << ": " << x << "\n";
+#define space " "
+typedef long long int ll;
+typedef unsigned long long int ull;
+template <typename type> void print(const vector<vector<type> > &arr);
+template <typename type> void print(const vector<type> &arr);
+template <typename type> void off_print(const vector<type> &arr);
+template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr);
+template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr);
+template <typename t1, typename t2> void print(const pair<t1, t2> &p);
+
+vector<ll> arr;
+vector<ll> prefix_sum;
+void solve(){
+    int n;
+    cin >> n;
+    arr.clear();
+    arr.resize(n);
+    forn(i, n) cin >> arr[i];
+    ll sum = accumulate(arr.begin(), arr.end(), ((ll)0));
+    if (sum != 0){
+        cout << "No\n";
+        return;
+    }
+    prefix_sum.clear();
+    prefix_sum.resize(n);
+    prefix_sum[0] = arr[0];
+    if (prefix_sum[0] < 0){
+        cout << "No\n";
+        return;
+    }
+    if (prefix_sum[0] == 0){
+        forsn(j, 1, n-1){
+            if (arr[j] != 0){
+                cout << "No\n";
+                return;
+            }
+        }
+        cout << "Yes\n";
+        return;
+    }
+    forsn(i, 1, n-1){
+        prefix_sum[i] = prefix_sum[i-1] + arr[i];
+        if (prefix_sum[i] < 0){
+            cout << "No\n";
+            return;
+        }
+        if (prefix_sum[i] == 0){
+            forsn(j, i + 1, n-1){
+                if (arr[j] != 0){
+                    cout << "No\n";
+                    return;
+                }
+            }
+            cout << "Yes\n";
+            return;
+        }
+
+    }
+    cout << "Yes\n";
+}
+
+int main(){
+	fastIO;
+	TEST;
+	return 0;
+}
+
+
+template <typename type> void print(const vector<vector<type> > &arr){
+	cout << "\n[";
+	forn(i, arr.size()){
+		cout << "[";
+		forn(j, arr[i].size() - 1)
+			cout << arr[i][j] << ", ";
+		cout << arr[i][arr[i].size() - 1] << "]";
+		if (i != arr.size() - 1)
+			cout << "," << endl;
+	}
+	cout << "], 2D Vector\n";
+}
+
+
+template <typename type> void print(const vector<type> &arr){
+	cout << "\n[";
+	forn(i, arr.size()){
+		cout << arr[i];
+		if (i != arr.size() - 1)
+			cout << ", ";
+	}
+	cout << "], 1D Vector\n";
+}
+
+template <typename type> void off_print(const vector<type> &arr){
+	forn(i, arr.size())
+		cout << arr[i] << " ";
+}
+
+
+template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr){
+	int n = arr.size();
+	cout << "\n[";
+	forn(i, n - 1){
+		cout << "{" << arr[i].first << "," << arr[i].second << "}, ";
+	}
+	cout << "{" << arr[n - 1].first << "," << arr[n - 1].second << "}], 1D Vector of Pairs\n";
+}
+
+
+template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr){
+	cout << "\n[";
+	forn(i, arr.size()){
+		cout << "[";
+		forn(j, arr[i].size()){
+			cout << "{" << arr[i][j].first << "," << arr[i][j].second << "}";
+			if (j != arr[i].size() - 1)
+				cout << ", ";
+		}
+		cout << "]";
+		if (i != arr.size() - 1)
+			cout << "," << endl;
+	}
+	cout << "], 2D Vector of Pairs\n";
+}
+
+
+template <typename t1, typename t2> void print(const pair<t1, t2> &p){
+	cout << "\n{" << p.first << "," << p.second << "}, Pair\n";
+}
