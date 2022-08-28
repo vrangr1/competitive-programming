aedf2151a567d7eb81030f2e94eb6eca77340151
anavp <anavprasadanav@gmail.com>
2022-08-28 15:10:58 -0400

    my oa citadel question added

--- mathworks/sum_as_you_go.cpp
+++ mathworks/sum_as_you_go.cpp
@@ -15,8 +15,8 @@
 #include <climits>
 #include <numeric>
 #include <cmath>
-#include <queue>
 #include <assert.h>
+#include <queue>
 
 using namespace std;
 
@@ -47,13 +47,74 @@ template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr);
 template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr);
 template <typename t1, typename t2> void print(const pair<t1, t2> &p);
 
-void solve(){
+inline int getMax(deque<pair<int, int> > &prev, int curInd, int maxLength){
+    if (curInd - prev.front().second > maxLength){
+        prev.pop_front();
+        // return prev.front().first;
+    }
+    return prev.front().first;
+}
 
+void update_deque(deque<pair<int, int> > &prev, int newVal, int ind){
+    while(prev.size() > 0 && prev.back().first <= newVal){
+        prev.pop_back();
+    }
+    prev.push_back(make_pair(newVal, ind));
+}
+
+template<typename type> void print_deque(deque<type> &deq){
+    for (auto iter = deq.rbegin(); iter != deq.rend(); ++iter)
+        print(*iter);
+}
+
+int max_sum(vector<int> &arr, int maxLength){
+    int n = arr.size();
+    // int sum = arr[0], maxsum = arr[0];
+    deque<pair<int, int> > prevValues;
+    vector<int> dp(n, -1);
+    dp[0] = arr[0];
+    prevValues.push_back(make_pair(arr[0], 0));
+    for (int i = 1; i < n; ++i){
+        dp[i] = max(getMax(prevValues, i, maxLength) + arr[i], arr[i]);
+        print_var(dp[i]);
+        print_var(i);
+        cout << "before\n";
+        print_deque(prevValues);
+        update_deque(prevValues, dp[i], i);
+        cout << "after\n";
+        print_deque(prevValues);
+        cout << "\n\n\n";
+    }
+    print(dp);
+    return *max_element(dp.begin(), dp.end());
+    // for (int i = 1; i < n; ++i){
+    //     sum += arr[i];
+    //     maxsum = max(sum, maxsum);
+    //     if (sum <= 0) sum = 0;
+    // }
+    return 0;
+}
+
+
+void solve(){
+    // vector<int> arr{100, -70, -90, -80, 100};
+    vector<int> arr;
+    // arr.push_back(100);
+    // arr.push_back(-70);
+    // arr.push_back(-90);
+    // arr.push_back(-80);
+    // arr.push_back(100);
+    arr.push_back(3);
+    arr.push_back(-4);
+    arr.push_back(-3);
+    arr.push_back(-5);
+    arr.push_back(0);
+    cout << "ans: " << max_sum(arr, 2) << endl;
 }
 
 int main(){
 	fastIO;
-	TEST;
+	TEST1;
 	return 0;
 }
 
