f41085df8e4f4e42d1429ea68a56b58ff9ceead9
vrangr1 <anavprasadanav@gmail.com>
2023-10-02 00:09:48 -0400

    codeforces remnant: round 901; finished problem d; another doable problem (albeit with a little difficulty)

--- codeforces/codeforces_901_div2/d_jellyfish_mex.cpp
+++ codeforces/codeforces_901_div2/d_jellyfish_mex.cpp
@@ -52,7 +52,7 @@ typedef long long int ll;
 typedef unsigned long long int ull;
 #define endl "\n"
 #define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
-#define TEST int T;cin>>T;while(T--)solve();
+#define TEST ll T;cin>>T;while(T--)solve();
 #define TEST1 solve();
 #define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
 #define rep(...) GET_MACRO(__VA_ARGS__, forsn, qwe, forn)(__VA_ARGS__)
@@ -75,26 +75,58 @@ int main(){
 	return 0;
 }
 
-ll get_count(ull n, ull m){
-    if (n == m) return 0;
-    ll gcd = __gcd(n,m);
-    ll p = n/gcd, q = m/gcd;
-    if ((q-(q&(-q))) != 0) return -1;
-    debug(p,q);
-    ll bit, sol = 0ll;
-    while(p>0ll){
-        bit = (p&(-p));
-        p-=bit;
-        assert(q%bit == 0ll);
-        bit = q/bit;
-        sol += (bit-1ll)*(m/bit);
-    }
-    return sol;
+ll get_mex(const vector<ll> &counts){
+    for (ll i = 0; i < counts.size(); ++i)
+        if (counts[i] == 0) return i;
+    assert(false);
+    return -1;
 }
 
 void solve(){
-    ll n, m; cin >> n >> m;
-    if (m%2 && n%m) return void(cout << "-1\n");
-    if (n%m == 0) return void(cout << "0\n");
-    cout << get_count(n%m,m) << endl;
+    ll n; cin >> n;
+    vector<ll> a(n), counts(n+1,0);
+    rep(i,n){
+        cin >> a[i];
+        if (a[i] <= n) counts[a[i]]++;
+    }
+    ll mex = get_mex(counts), m;
+    ll minm = mex*(counts[0]-1ll);
+    if (minm == 0) return void(cout << "0\n");
+    vector<ll> dp(mex,0);
+    dp[0] = 0;
+    dp[1] = counts[0]-1ll;
+    for (ll i = 2; i < mex; ++i){
+        dp[i] = i*(counts[0]-1ll);
+        for (ll j = 1; j < i; ++j)
+            dp[i] = min(dp[i], i*(counts[j]-1ll) + j + dp[j]);
+    }
+    debug(counts, mex, dp, minm);
+    for (ll i = 1; i < mex; ++i){
+        minm = min(mex*(counts[i]-1ll) + i + dp[i], minm);
+    }
+    cout << minm << endl;
 }
+
+
+
+/*
+
+ 0000000 1111 22 333 444 ... .. .. ... mex
+  0 -> mex * (ct0-1)
+  1 -> mex * (ct1-1) + 1 + ct0-1
+  2 -> mex * (ct2-1) + 2 + min(2*(ct0-1) + 0, 2*(ct1-1) + 1 + ct0)
+  3 -> mex * ct3 + min(3*ct2 + min(2*ct0, 2*ct1 + ct0), 3*ct1 + ct0, 3*ct0)
+  4 -> mex * ct4 + min(4*ct3 + min(3*ct2 + min(2*ct0, 2*ct1 + ct0), 3*ct1 + ct0, 3*ct0), 4*ct2 +...)
+
+  mex*cti + 
+  0 -> 0
+  1 -> ct0-1
+  2 -> min(2*(ct0-1) + 0, 2*(ct1-1) + 1 + ct0-1) = min(2*(ct0-1) + 0, 2*(ct1-1) + 1 + v1)
+  3 -> min(3*(ct2-1) + 2 + v2, 3*ct1 + v1, 3*ct0)
+  4 -> min(4*ct3 + v3, 4*ct2 + v2, 4*ct1 + v1, 4*ct0)
+  
+*/
+
+// 1
+// 8
+// 5 2 1 0 3 0 4 0
