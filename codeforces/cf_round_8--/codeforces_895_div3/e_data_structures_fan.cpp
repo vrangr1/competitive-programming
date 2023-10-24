84ccc2423ad39fee91e76f0be51decd2bf6ac91a
vrangr1 <anavprasadanav@gmail.com>
2023-10-24 11:57:34 +0530

    codeforces remnant: 895 div3; solved problem E

--- codeforces/cf_round_8--/codeforces_895_div3/e_data_structures_fan.cpp
+++ codeforces/cf_round_8--/codeforces_895_div3/e_data_structures_fan.cpp
@@ -1,7 +1,12 @@
 #if 0
     me=`basename $0 .cpp`
     rm -f $me $me.out
-    g++ -std=c++20 -DLOCAL $me.cpp -o $me
+    only_compile=${1:-0}
+    if [ $only_compile == compile ]; then
+        g++ -std=c++20 $me.cpp -o $me -Wall -O2 -Wextra -Wno-sign-conversion -Wshadow
+        exit
+    fi
+    g++ -std=c++20 -DLOCAL $me.cpp -o $me -Wall -O2 -Wextra -Wno-sign-conversion -Wshadow
     if test -f $me; then
 	    ./$me > $me.out
         echo "\noutput begins now:"
@@ -12,7 +17,7 @@
 #endif
 /***************************************************
 * AUTHOR : Anav Prasad
-* Nick : vrangr
+* Nick   : vrangr
 ****************************************************/
 #include <iostream>
 #include <fstream>
@@ -36,77 +41,73 @@
 #include <unordered_map>
 #include <bit>
 #include <bitset>
+#include <random>
 #include <assert.h>
-#define debug
+#define debug(...)
 #ifdef LOCAL
     #undef debug
     #include <algo/debug.hpp>
     const bool DEBUG = true;
-#else
-    const bool DEBUG = false;
 #endif
 
 using namespace std;
 
+typedef long long int ll;
+typedef unsigned long long int ull;
 #define endl "\n"
 #define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
 #define TEST int T;cin>>T;while(T--)solve();
 #define TEST1 solve();
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, qwe, forn)(__VA_ARGS__)
+#define repll(...) GET_MACRO(__VA_ARGS__, forsnll, qwe, fornll)(__VA_ARGS__)
+#define qwe(r,t,y)
 #define forn(i, n) for (int i = 0; i < n; i++)
-#define forsn(i, st_val, n) for (int i = st_val; i <= n; ++i)
-#define forr(i, n) for (int i = n - 1; i >= 0; --i)
-#define forsr(i, st_val, n) for (int i = st_val; i >= n; --i)
-#define pb1(a) push_back(a)
-#define pb2(a,b) push_back({a, b})
-#define GET_MACRO(_1,_2,_3,NAME,...) NAME
-#define pb(...) GET_MACRO(__VA_ARGS__, pb1, pb2)(__VA_ARGS__)
+#define fornll(i, n) for (ll i = 0ll; i < n; i++)
+#define forsn(i, st, end, d) for(int i = st; (d>0?i<=end:i>=end); i+=d)
+#define forsnll(i, st, end, d) for(ll i = st; (d>0?i<=end:i>=end); i+=(ll)d)
+#define all(x) (x).begin(), (x).end()
 #define pass (void)0
 #define space " "
 #define yes "YES\n"
 #define no "NO\n"
-typedef long long int ll;
-typedef unsigned long long int ull;
 void solve();
 
 int main(){
 	fastIO;
 	TEST;
+    #ifdef LOCAL
+        cout << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
 	return 0;
 }
 
 void solve(){
     int n; cin >> n;
     vector<int> a(n);
-    forn(i,n) cin >> a[i];
+    rep(i,n) cin >> a[i];
     string s;
     cin >> s;
-    int xor1 = 0, xor0 = 0;
-    vector<int> prefix_xor0(n,0), prefix_xor1(n,0);
-    prefix_xor1[0] = (s[0] == '1'? a[0] : 0);
-    prefix_xor0[0] = (s[0] == '1'? 0 : a[0]);
-    forsn(i, 1, n-1){
-        prefix_xor0[i] = prefix_xor0[i-1];
-        prefix_xor1[i] = prefix_xor1[i-1];
-        if (s[i] == '1')
-            prefix_xor1[i] ^= a[i];
-        else prefix_xor0[i] ^= a[i];
-    }
-    xor0 = prefix_xor0[n-1];
-    xor1 = prefix_xor1[n-1];
-    int q;
-    cin >> q;
-    forn(i,q){
-        int tp; cin >> tp;
-        int l,r,g;
-        switch(tp){
-            case 1:
-                cin >> l >> r;
-                
-                break;
-            case 2:
-                cin >> g;
-
-                break;
+    int q; cin >> q;
+    vector<int> pxor(n);
+    partial_sum(all(a), pxor.begin(), [](const int cur, const int &val){
+        return cur^val;
+    });
+    int soln = 0;
+    rep(i,n)
+        if(s[i] == '1') soln ^= a[i];
+    while(q--){
+        int t; cin >> t;
+        int l, r, g;
+        if (t == 1){
+            cin >> l >> r; --l; --r;
+            soln ^= (pxor[r]^pxor[l]^a[l]);
+            continue;
+        }
+        else{
+            cin >> g;
+            if (g == 1) cout << soln << " \n"[q==0];
+            else cout << (pxor[n-1]^soln) << " \n"[q==0];
         }
     }
 }
