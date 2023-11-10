87e7f49c11b3896cfc376c4ce349755d2df7f622
vrangr1 <anavprasadanav@gmail.com>
2023-11-11 03:09:21 +0530

    lots of scripts and test template updates

--- algo/generator.cpp
+++ algo/generator.cpp
@@ -37,23 +37,27 @@ using namespace std;
 
 typedef long long int ll;
 typedef unsigned long long int ull;
+typedef long double ld;
 #define endl "\n"
 #define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
 #define TEST cout << "1\n"
 #define TEST1
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
 #define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
 #define rep(...) GET_MACRO(__VA_ARGS__, forsn, qwe, forn)(__VA_ARGS__)
 #define repll(...) GET_MACRO(__VA_ARGS__, forsnll, qwe, fornll)(__VA_ARGS__)
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define fornll(i, n) for (ll i = 0ll; i < (ll)n; i++)
+#define forsn(i, st, end, d) for(int i = st; (d>0?i<=(int)end:i>=(int)end); i+=d)
+#define forsnll(i, st, end, d) for(ll i = st; (d>0?i<=(ll)end:i>=(ll)end); i+=(ll)d)
 #define qwe(r,t,y)
-#define forn(i, n) for (int i = 0; i < n; i++)
-#define fornll(i, n) for (ll i = 0ll; i < n; i++)
-#define forsn(i, st, end, d) for(int i = st; (d>0?i<=end:i>=end); i+=d)
-#define forsnll(i, st, end, d) for(ll i = st; (d>0?i<=end:i>=end); i+=(ll)d)
-#define all(x) (x).begin(), (x).end()
-#define pass (void)0
 #define space " "
 #define yes "YES\n"
 #define no "NO\n"
+#define pass (void)0
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 ull grng(const ull maxval){return rng()%maxval;}
 void generate_test();
