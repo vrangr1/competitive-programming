d648db61cbcc3352866c799b2a31621db454b7ad
vrangr1 <anavprasadanav@gmail.com>
2023-11-15 15:54:11 +0530

    minor template update

--- algo/template.cpp
+++ algo/template.cpp
@@ -38,6 +38,7 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
 typedef long double ld;
+typedef __int128_t i128;
 #define endl "\n"
 #define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
 #define TEST int T;cin>>T;while(T--)solve();
@@ -46,13 +47,9 @@ typedef long double ld;
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(v) ((int)(v).size())
 #define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
-#define rep(...) GET_MACRO(__VA_ARGS__, forsn, qwe, forn)(__VA_ARGS__)
-#define repll(...) GET_MACRO(__VA_ARGS__, forsnll, qwe, fornll)(__VA_ARGS__)
-#define forn(i, n) for (int i = 0; i < (int)n; i++)
-#define fornll(i, n) for (ll i = 0ll; i < (ll)n; i++)
-#define forsn(i, st, end, d) for(int i = st; (d>0?i<=(int)end:i>=(int)end); i+=d)
-#define forsnll(i, st, end, d) for(ll i = st; (d>0?i<=(ll)end:i>=(ll)end); i+=(ll)d)
-#define qwe(r,t,y)
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
 #define space " "
 #define yes "YES\n"
 #define no "NO\n"
