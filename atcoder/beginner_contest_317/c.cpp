21a333e99d96c669d29d8d6df55dd29ac4b426e4
vrangr1 <anavprasadanav@gmail.com>
2023-11-19 00:20:33 +0530

    atcoder practice: abc 317; solved problem c again

---
+++ atcoder/beginner_contest_317/c.cpp
@@ -0,0 +1,96 @@
+/***************************************************
+* AUTHOR : Anav Prasad
+* Nick   : vrangr
+****************************************************/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <unordered_set>
+#include <set>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <unordered_map>
+#include <bit>
+#include <bitset>
+#include <random>
+#include <assert.h>
+#define debug(...) 42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#endif
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define space " "
+#define yes "YES\n"
+#define no "NO\n"
+#define pass (void)0
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+int main(){
+	fastIO;
+	TEST1;
+    #ifdef LOCAL
+        cout << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+void solve(){
+    int n, m; cin >> n >> m;
+    vector<vector<vector<int>>> edges(n);
+    rep(i,m){
+        int u, v, w; cin >> u >> v >> w;
+        --u;--v;
+        edges[u].push_back({v,w});
+        edges[v].push_back({u,w});
+    }
+    int mxdist = 0;
+    int vis = 0;
+    auto brute_force = [&](auto self, int node, int curdist) -> void {
+        vis ^= (1<<node);
+        mxdist = max(curdist, mxdist);
+        for (auto &edge : edges[node]){
+            int ngb = edge[0];
+            int w = edge[1];
+            if ((vis&(1<<ngb))) continue;
+            self(self,ngb,curdist+w);
+        }
+        vis ^= (1<<node);
+    };
+    rep(i,n){
+        brute_force(brute_force, i, 0);
+        assert(vis == 0);
+    }
+    cout << mxdist << endl;
+}
