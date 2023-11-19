a3e3e641faa384c8bfd9b651503e4139d55bcf26
vrangr1 <anavprasadanav@gmail.com>
2023-11-19 20:04:44 +0530

    atcoder arc 168: horrible. solved only 1/6, too sleepy

---
+++ contests/atcoder/arc_168/a.cpp
@@ -0,0 +1,111 @@
+/***************************************************
+* AUTHOR : Anav Prasad
+* Nick   : vrangr
+****************************************************/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <unordered_set>
+#include <set>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <unordered_map>
+#include <bit>
+#include <bitset>
+#include <random>
+#include <assert.h>
+#define debug(...) 42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#endif
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define space " "
+#define yes "YES\n"
+#define no "NO\n"
+#define pass (void)0
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+int main(){
+	fastIO;
+	TEST1;
+    #ifdef LOCAL
+        cout << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+ll nc2(ll n){
+    return (n*(n-1ll))/2ll;
+}
+
+void solve(){
+    ll n; cin >> n;
+    string s; cin >> s;
+    ll ct = 0, cur = 0;
+    char last = '.';
+    rep(i,n-1ll){
+        if (s[i] == last){
+            cur += (last == '>');
+            continue;
+        }
+        if (last == '<'){
+            cur = 2ll;
+            last = '>';
+            continue;
+        }
+        else if (last == '>'){
+            ct += nc2(cur);
+            cur = 0;
+            last = '<';
+            continue;
+        }
+        if (s[i] == '>') cur = 2;
+        else cur = 0;
+        last = s[i];
+    }
+    ct += nc2(cur);
+    cout << ct << endl;
+}
+
+
+/*
+
+30
+<<><>>><><>><><><<>><<<><><<>
+
+00101110101101010011000101001
+
+
+*/
