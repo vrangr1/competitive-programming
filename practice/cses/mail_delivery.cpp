3008d578f13b2e7ff80cd224e8853443ef39f632
vrangr1 <anavprasadanav@gmail.com>
2023-11-29 19:27:15 +0530

    cses practice: mail delivery, eulerian circuit problem solved

---
+++ practice/cses/mail_delivery.cpp
@@ -0,0 +1,99 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Wed Nov 29 17:09:26 IST 2023
+****************************************************/
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <set>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <bit>
+#include <bitset>
+#include <array>
+#include <assert.h>
+#define debug(...) 42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#endif
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define space " "
+#define yes "YES\n"
+#define no "NO\n"
+#define pass (void)0
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main(){
+	fastIO;
+	TEST1;
+    #ifdef LOCAL
+        cout << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+void solve(){
+    int n, m; cin >> n >> m;
+    int u,v;
+    vector<vector<pair<int,int>>> edges(n);
+    vector<bool> vis(m,false);
+    vector<int> degree(n,0);
+    rep(i,m){
+        cin >> u >> v; --u;--v;
+        edges[u].push_back({v,i});
+        edges[v].push_back({u,i});
+        degree[u]++;
+        degree[v]++;
+    }
+    rep(i,n) if (degree[i]%2) return void(cout << "IMPOSSIBLE\n");
+    vector<int> path;
+    auto dfs = [&] (auto self, int node) -> void {
+        while(!edges[node].empty()){
+            auto [ngb, ind] = edges[node].back();
+            edges[node].pop_back();
+            if (vis[ind]) continue;
+            vis[ind] = true;
+            self(self, ngb);
+        }
+        path.push_back(node+1);
+    };
+    dfs(dfs,0);
+    if (sz(path) != m+1) return void(cout << "IMPOSSIBLE\n");
+    print_vec(path);
+}
