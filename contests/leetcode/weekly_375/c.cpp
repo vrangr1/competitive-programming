a2519761724e8731d36d61e88dd0785a07979720
vrangr1 <anavprasadanav@gmail.com>
2023-12-10 09:08:58 +0530

    leetcode weekly 375; problem c

---
+++ contests/leetcode/weekly_375/c.cpp
@@ -0,0 +1,91 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Sun Dec 10 08:12:13 IST 2023
+****************************************************/
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <set>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <bit>
+#include <bitset>
+#include <array>
+#include <assert.h>
+#define debug(...) 42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#endif
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define space " "
+#define yes "YES\n"
+#define no "NO\n"
+#define pass (void)0
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main(){
+	fastIO;
+	TEST1;
+    #ifdef LOCAL
+        cout << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+ll countSubarrays(vector<ll>& nums, ll k) {
+    const ll mx = *max_element(all(nums)), n = nums.size();
+    ll l = 0, r = 0, ct = 0, prev, oct = 0;
+    while(r < n){
+        while(r < n && ct < k){
+            ct += (nums[r++]==mx);
+        }
+        prev = l;
+        while(l < r && ct == k){
+            ct -= (nums[l++]==mx);
+        }
+        oct += (l-prev)*(n-r+1ll);
+    }
+    return oct;
+}
+
+void solve(){
+    ll n,k; cin >> n >> k;
+    vector<ll> a(n); rep(i,n) cin >> a[i];
+    cout << countSubarrays(a,k) << endl;
+}
---
+++ contests/leetcode/weekly_375/test_c/brute_force.cpp
@@ -0,0 +1,102 @@
+/***************************************************
+* AUTHOR : Anav Prasad
+* Nick   : vrangr
+****************************************************/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <unordered_set>
+#include <set>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <unordered_map>
+#include <bit>
+#include <bitset>
+#include <random>
+#include <assert.h>
+#define debug(...)
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#endif
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, qwe, forn)(__VA_ARGS__)
+#define repll(...) GET_MACRO(__VA_ARGS__, forsnll, qwe, fornll)(__VA_ARGS__)
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define fornll(i, n) for (ll i = 0ll; i < (ll)n; i++)
+#define forsn(i, st, end, d) for(int i = st; (d>0?i<=(int)end:i>=(int)end); i+=d)
+#define forsnll(i, st, end, d) for(ll i = st; (d>0?i<=(ll)end:i>=(ll)end); i+=(ll)d)
+#define qwe(r,t,y)
+#define space " "
+#define yes "YES\n"
+#define no "NO\n"
+#define pass (void)0
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+int main(){
+	fastIO;
+	TEST1;
+    #ifdef LOCAL
+        cout << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+void solve(){
+    ll n,k; cin >> n >> k;
+    vector<ll> a(n); rep(i,n) cin >> a[i];
+    vector<ll> psum(n+1ll);
+    ll mx = *max_element(all(a));
+    psum[0] = 0ll;
+    rep(i,1,n,1ll)
+        psum[i] = psum[i-1]+(ll)(a[i-1ll]==mx);
+    ll oct = 0ll;
+    rep(i,n){
+        rep(j,i,n-1ll,1ll){
+            if (psum[j+1ll] - psum[i] >= k){
+                oct++;
+            }
+        }
+    }
+    cout << oct << endl;
+}
+
+/*
+5 2
+1 3 2 3 3
+
+4 3
+1 4 2 1
+
+22 1
+4 3 4 10 4 7 9 2 10 6 10 4 2 7 10 10 10 5 10 5 6 7
+
+
+*/
