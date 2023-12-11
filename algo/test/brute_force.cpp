703cb4799b280c05130c8844ab93a150d8efe1bd
vrangr1 <anavprasadanav@gmail.com>
2023-12-11 14:49:35 +0530

    algo/test updates

--- algo/test/brute_force.cpp
+++ algo/test/brute_force.cpp
@@ -3,11 +3,9 @@
 * Nick   : vrangr
 ****************************************************/
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string.h>
 #include <ctype.h>
-#include <unordered_set>
 #include <set>
 #include <stdlib.h>
 #include <map>
@@ -21,10 +19,9 @@
 #include <stack>
 #include <list>
 #include <forward_list>
-#include <unordered_map>
 #include <bit>
 #include <bitset>
-#include <random>
+#include <array>
 #include <assert.h>
 #define debug(...)
 #ifdef LOCAL
@@ -38,6 +35,7 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
 typedef long double ld;
+typedef __int128_t i128;
 #define endl "\n"
 #define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
 #define TEST int T;cin>>T;while(T--)solve();
@@ -46,13 +44,9 @@ typedef long double ld;
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(v) ((int)(v).size())
 #define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
-#define rep(...) GET_MACRO(__VA_ARGS__, forsn, qwe, forn)(__VA_ARGS__)
-#define repll(...) GET_MACRO(__VA_ARGS__, forsnll, qwe, fornll)(__VA_ARGS__)
-#define forn(i, n) for (int i = 0; i < (int)n; i++)
-#define fornll(i, n) for (ll i = 0ll; i < (ll)n; i++)
-#define forsn(i, st, end, d) for(int i = st; (d>0?i<=(int)end:i>=(int)end); i+=d)
-#define forsnll(i, st, end, d) for(ll i = st; (d>0?i<=(ll)end:i>=(ll)end); i+=(ll)d)
-#define qwe(r,t,y)
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
 #define space " "
 #define yes "YES\n"
 #define no "NO\n"
--- algo/test/generator.cpp
+++ algo/test/generator.cpp
@@ -3,11 +3,9 @@
 * Nick   : vrangr
 ****************************************************/
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string.h>
 #include <ctype.h>
-#include <unordered_set>
 #include <set>
 #include <stdlib.h>
 #include <map>
@@ -21,9 +19,9 @@
 #include <stack>
 #include <list>
 #include <forward_list>
-#include <unordered_map>
 #include <bit>
 #include <bitset>
+#include <array>
 #include <random>
 #include <assert.h>
 #define debug(...)
@@ -38,6 +36,7 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
 typedef long double ld;
+typedef __int128_t i128;
 #define endl "\n"
 #define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
 #define TEST cout << "1\n"
@@ -46,19 +45,15 @@ typedef long double ld;
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(v) ((int)(v).size())
 #define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
-#define rep(...) GET_MACRO(__VA_ARGS__, forsn, qwe, forn)(__VA_ARGS__)
-#define repll(...) GET_MACRO(__VA_ARGS__, forsnll, qwe, fornll)(__VA_ARGS__)
-#define forn(i, n) for (int i = 0; i < (int)n; i++)
-#define fornll(i, n) for (ll i = 0ll; i < (ll)n; i++)
-#define forsn(i, st, end, d) for(int i = st; (d>0?i<=(int)end:i>=(int)end); i+=d)
-#define forsnll(i, st, end, d) for(ll i = st; (d>0?i<=(ll)end:i>=(ll)end); i+=(ll)d)
-#define qwe(r,t,y)
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
 #define space " "
 #define yes "YES\n"
 #define no "NO\n"
 #define pass (void)0
 template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937 rng((uint)chrono::steady_clock::now().time_since_epoch().count());
 ull grng(const ull maxval){return rng()%maxval;}
 void generate_test();
 
