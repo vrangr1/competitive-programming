57663c7ceae3f9515e5a6ad07ee73a7faadf9e98
vrangr1 <anavprasadanav@gmail.com>
2023-12-23 02:25:54 +0530

    cf practice: 514 div2: solved problem c

---
+++ contests/codeforces/cf_round_5--/cf_514_div2/c.cpp
@@ -0,0 +1,99 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Sat Dec 23 01:52:44 IST 2023
+****************************************************/
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <set>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <bit>
+#include <bitset>
+#include <array>
+#include <assert.h>
+#define debug(...) 42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define space " "
+#define yes "YES\n"
+#define no "NO\n"
+#define pass ((void)0)
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main(){
+	fastIO;
+	TEST1;
+    #ifdef LOCAL
+        cout << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+void solve(){
+    int n; cin >> n;
+    auto compute = [] (int n) -> vector<int> {
+        if (n == 1) return {1};
+        if (n == 3) return {1,1,3};
+        vector<int> sol;
+        int cur = 1;
+        while(n > 3){
+            rep(i,(n+1)/2) sol.push_back(cur);
+            cur<<=1;
+            n>>=1;
+        }
+        if (n == 3){
+            vector<int> t = {cur,cur,3*cur};
+            sol.insert(sol.end(), all(t));
+        }
+        else if (n == 2){
+            sol.push_back(cur);
+            sol.push_back(2*cur);
+        }
+        else sol.push_back(cur);
+        return sol;
+    };
+    vector<int> sol = compute(n);
+    print_vec(sol);
+}
