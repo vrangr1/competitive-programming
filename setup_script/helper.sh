1cae7982b020e60eeb1125b3c28fe2a088cc1bde
vrangr1 <anavprasadanav@gmail.com>
2024-03-09 04:30:15 +0530

    scripts: formatting updates

---
+++ setup_script/helper.sh
@@ -0,0 +1,41 @@
+### Text formatting ###
+
+CLEAR="\033[0m"
+
+# Text settings.
+BOLD="\033[1m"
+UNDERLINE="\033[4m"
+
+# Text color.
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+
+# Text color with bold font.
+RED_BOLD="\033[1;31m"
+GREEN_BOLD="\033[1;32m"
+YELLOW_BOLD="\033[1;33m"
+BLUE_BOLD="\033[1;34m"
+MAGENTA_BOLD="\033[1;35m"
+CYAN_BOLD="\033[1;36m"
+
+# Background color.
+RED_BG="\033[41m"
+GREEN_BG="\033[42m"
+YELLOW_BG="\033[43m"
+BLUE_BG="\033[44m"
+MAGENTA_BG="\033[45m"
+CYAN_BG="\033[46m"
+
+# Background color with bold font.
+RED_BG_BOLD="\033[1;41m"
+GREEN_BG_BOLD="\033[1;42m"
+YELLOW_BG_BOLD="\033[1;43m"
+BLUE_BG_BOLD="\033[1;44m"
+MAGENTA_BG_BOLD="\033[1;45m"
+CYAN_BG_BOLD="\033[1;46m"
+
+### End of text formatting ###
