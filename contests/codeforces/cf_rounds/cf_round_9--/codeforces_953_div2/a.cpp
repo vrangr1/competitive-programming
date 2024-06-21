a3914e92fb2e2a51eb85920d6bd75c73a4780028
vrangr1 <anavprasadanav@gmail.com>
2024-06-21 18:31:43 +0530

    cf round 953 div2: solved 2/6 :(

---
+++ contests/codeforces/cf_rounds/cf_round_9--/codeforces_953_div2/a.cpp
@@ -0,0 +1,59 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Sun Jun 16 14:34:11 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    int ind = (int)(max_element(a.begin(),a.begin()+(n-1)) - a.begin());
+    cout << a[ind] + a.back() << endl;
+}
