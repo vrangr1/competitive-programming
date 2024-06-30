4b65e953f4b1b547a71b4999bae2d94513cc584f
vrangr1 <anavprasadanav@gmail.com>
2024-06-30 20:04:51 +0530

    atcoder abc 360: solved 4/7

---
+++ contests/atcoder/beginner_contests/abc_360/d.cpp
@@ -0,0 +1,78 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Sun Jun 30 17:41:43 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST1;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    ll n, t; cin >> n >> t;
+    string s; cin >> s;
+    vector<ll> x(n);
+    rep(i,n) cin >> x[i];
+    vector<ll> order(n);
+    iota(all(order),0ll);
+    sort(all(order),[&](const ll &i, const ll &j) {
+        return x[i] < x[j];
+    });
+    multiset<ll> dest;
+    ll sol = 0ll;
+    rep(ind,n-1,0ll,-1) {
+        ll i = order[ind];
+        if (s[i] == '0') {
+            dest.insert(x[i]-t);
+            continue;
+        }
+        ll r = x[i]+t;
+        while(!dest.empty() && *dest.rbegin() > r) {
+            dest.erase(prev(dest.end()));
+        }
+        sol += (ll)sz(dest);
+    }
+    cout << sol << endl;
+}
