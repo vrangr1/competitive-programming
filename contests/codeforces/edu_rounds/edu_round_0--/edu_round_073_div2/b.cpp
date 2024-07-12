f65291c591fbea0f74fcad463d4bfbc6bcfba772
vrangr1 <anavprasadanav@gmail.com>
2024-07-12 18:19:24 +0530

    cf edu round 73 div2: problem b solved

---
+++ contests/codeforces/edu_rounds/edu_round_0--/edu_round_073_div2/b.cpp
@@ -0,0 +1,82 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Fri Jul 12 17:12:32 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST1;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<string> sol(n,string(n,'.'));
+    auto duel = [&](auto &&self, int x, int y) -> void {
+        assert(sol[x][y] != '.');
+        char t = (sol[x][y] == 'B'?'W':'B');
+        rep(dx,-2,2,1) {
+            if (dx == 0) continue;
+            int cy = (abs(dx) == 2? 1: 2);
+            rep(dy,-cy,cy,cy) {
+                if (dy == 0) continue;
+                int nx = x+dx, ny = y+dy;
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                if (sol[nx][ny] != '.') {
+                    assert(sol[nx][ny] == t);
+                    continue;
+                }
+                sol[nx][ny] = t;
+                self(self,nx,ny);
+            }
+        }
+    };
+    rep(i,n)
+        rep(j,n)
+            if (sol[i][j] == '.') {
+                sol[i][j] = 'W';
+                duel(duel,i,j);
+            }
+    rep(i,n) cout << sol[i] << endl;
+}
