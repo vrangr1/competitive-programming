69b83a55c81b00f15d05ecd48f90cfadd58b6175
vrangr1 <anavprasadanav@gmail.com>
2024-07-15 23:02:57 +0530

    cf 958 div2: solved 3/6

---
+++ contests/codeforces/cf_rounds/cf_round_9--/codeforces_958_div2/d.cpp
@@ -0,0 +1,131 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Mon Jul 15 20:32:19 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    ll n; cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    vector<vector<ll>> g(n);
+    rep(i,n-1ll) {
+        ll u, v; cin >> u >> v;
+        g[--u].push_back(--v);
+        g[v].push_back(u);
+    }
+    ll sol = LLONG_MAX;
+    {
+        ll cur = 0ll;
+        auto dfs = [&](auto &&self, ll u, ll p, bool sel = true) -> void {
+            if (sel) cur += a[u];
+            for (ll &v : g[u]) {
+                if (v == p) continue;
+                self(self,v,u,!sel);
+            }
+        };
+        dfs(dfs,0,-1);
+        sol = cur;
+        cur = 0ll;
+        dfs(dfs,0,-1,false);
+        sol = min(sol+2ll*cur,sol*2ll + cur);
+    }
+    ll sum = accumulate(all(a),0ll), cur = 0ll;
+    vector<array<ll,2>> dp(n), sct(n);
+    auto dfs =[&](auto &&self, ll u, ll p) -> void {
+        sct[u][1] = 1ll;
+        dp[u][1] = a[u];
+        for (ll &v : g[u]) {
+            if (v == p) continue;
+            self(self, v, u);
+            dp[u][1] += dp[v][0];
+            dp[u][0] += max(dp[v][1],dp[v][0]);
+            sct[u][1] += sct[v][0];
+            if (dp[v][1] == dp[v][0]) sct[u][0] = max(sct[v][0],sct[v][1]);
+            else if (dp[v][1] > dp[v][0]) sct[u][0] = sct[v][1];
+            else sct[u][0] = sct[v][0];
+        }
+    };
+    auto cleanup = [&](auto &&self, ll u, ll p, bool sel) -> void {
+        if (a[u] == 0ll) assert(!sel);
+        if (sel) a[u] = 0;
+        for (ll &v : g[u]) {
+            if (v == p) continue;
+            if (sel) self(self, v, u, !sel);
+            else {
+                if (a[v] == 0ll) self(self, v, u, false);
+                else if (sct[v][1] < sct[v][0]) self(self,v, u, false);
+                else self(self, v, u, true);
+            }
+        }
+    };
+    while(true) {
+        cur += sum;
+        if (cur >= sol) break;
+        fill(all(dp),array<ll,2>{0,0});
+        fill(all(sct),array<ll,2>{0,0});
+        dfs(dfs,0,-1ll);
+        // debug(dp,sct);
+        sum -= max(dp[0][0],dp[0][1]);
+        if (dp[0][0] > dp[0][1]) cleanup(cleanup,0,-1,false);
+        else if (dp[0][0] < dp[0][1]) cleanup(cleanup,0,-1,true);
+        else if (a[0] == 0ll) cleanup(cleanup,0,-1,false);
+        else if (sct[0][1] >= sct[0][0]) cleanup(cleanup,0,-1,true);
+        else cleanup(cleanup,0,-1,false);
+        // if (accumulate(all(a),0ll) != sum) {
+        //     debug('p');
+        //     exit(0);
+        // }
+        // assert(accumulate(all(a),0ll) == sum);
+        if (sum == 0ll) {
+            break;
+        }
+    }
+    cout << sol << endl;
+}
---
+++ contests/codeforces/cf_rounds/cf_round_9--/codeforces_958_div2/e.cpp
@@ -0,0 +1,345 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Mon Jul 15 21:30:30 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+// Imported code from Atcoder Library:
+#ifndef ATCODER_INTERNAL_BITOP_HPP
+#define ATCODER_INTERNAL_BITOP_HPP 1
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+namespace atcoder {
+
+namespace internal {
+
+// @param n `0 <= n`
+// @return minimum non-negative `x` s.t. `n <= 2**x`
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+// @param n `1 <= n`
+// @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+#endif  // ATCODER_INTERNAL_BITOP_HPP
+
+#ifndef ATCODER_LAZYSEGTREE_HPP
+#define ATCODER_LAZYSEGTREE_HPP 1
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+namespace atcoder {
+
+template <class S,
+          S (*op)(S, S),
+          S (*e)(),
+          class F,
+          S (*mapping)(F, S),
+          F (*composition)(F, F),
+          F (*id)()>
+struct lazy_segtree {
+  public:
+    lazy_segtree() : lazy_segtree(0) {}
+    lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
+    lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        lz = std::vector<F>(size, id());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return e();
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push(r >> i);
+        }
+
+        S sml = e(), smr = e();
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
+    }
+
+    S all_prod() { return d[1]; }
+
+    void apply(int p, F f) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = mapping(f, d[p]);
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return;
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        {
+            int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
+
+    template <bool (*g)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return g(x); });
+    }
+    template <class G> int max_right(int l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (int i = log; i >= 1; i--) push(l >> i);
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*g)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return g(x); });
+    }
+    template <class G> int min_left(int r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+    std::vector<F> lz;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, F f) {
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id();
+    }
+};
+
+}  // namespace atcoder
+
+#endif  // ATCODER_LAZYSEGTREE_HPP
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+class S {
+public:
+    ll v;
+    S() : v(0) {}
+    S(ll val) {
+        v = val;
+    }
+};
+
+S op(S a, S b) {
+    return S(a.v + b.v);
+}
+
+S e() {
+    return S(0);
+}
+
+class F {
+public:
+    ll v;
+    F() : v(0) {}
+    F(ll val) {
+        v = val;
+    }
+};
+
+S mapping(F f, S x) {
+    return S(f.v + x.v);
+}
+
+F comp(F f, F g) {
+    return F(f.v + g.v);
+}
+
+F id() {
+    return F(0);
+}
+
+void solve() {
+    ll n; cin >> n;
+    vector<ll> p(n);
+    rep(i,n) cin >> p[i];
+    set<ll> st = {-1ll,n};
+    vector<ll> order(n);
+    iota(all(order),0);
+    sort(all(order),[&](const ll &i, const ll &j) {
+        return p[i] < p[j];
+    });
+    auto get = [&](ll v) -> pair<ll,ll> {
+        assert(st.find(v) == st.end());
+        auto it = st.lower_bound(v);
+        assert(it != st.begin() && it != st.end());
+        return {*prev(it),*it};
+    };
+    vector<S> f(n,S(0));
+    atcoder::lazy_segtree<S, op, e, F, mapping, comp, id> seg(f);
+    rep(i,n) {
+        ll ind = order[i];
+        assert(p[ind] == i+1ll);
+        auto [l,r] = get(ind);
+        assert(l < ind);
+        assert(r > ind);
+        ll left = (ind-l), right = (r-ind);
+        if (i != n-1) {
+            seg.apply(i+1,n,F(left*right));
+        }
+        
+    }
+}
