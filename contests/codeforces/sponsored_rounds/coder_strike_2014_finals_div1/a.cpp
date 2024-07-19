71a3771a45ef7fd07846ea8d7621cc4dc59d8f91
vrangr1 <anavprasadanav@gmail.com>
2024-07-19 06:34:41 +0530

    cf coder strike 2014 finals div 1: solved problem A (simon daily problem)

---
+++ contests/codeforces/sponsored_rounds/coder_strike_2014_finals_div1/a.cpp
@@ -0,0 +1,62 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Fri Jul 19 06:28:34 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST1;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+// AHIMOTUVWXY
+set<char> st = {'A','H','I','M','O','T','U','V','W','X','Y'};
+void solve() {
+    string s; cin >> s;
+    for (char &ch : s) if (st.find(ch) == st.end()) return void(cout << no);
+    string t = s;
+    reverse(all(t));
+    if (s == t) cout << yes;
+    else cout << no;
+}
