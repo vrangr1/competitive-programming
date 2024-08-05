e05a187f5fabcb3b156c6770e65c1cd2df2745f2
vrangr1 <anavprasadanav@gmail.com>
2024-08-06 04:33:19 +0530

    cf 953 div 2: solved problem d

---
+++ contests/codeforces/cf_rounds/cf_round_9--/codeforces_953_div2/d.cpp
@@ -0,0 +1,75 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Tue Aug  6 04:17:29 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    ll n, c; cin >> n >> c;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    vector<ll> psum(n);
+    partial_sum(all(a),psum.begin());
+    ll mx = ll(max_element(all(a))-a.begin());
+    if (a[0] + c >= a[mx]) mx = 0ll;
+    vector<ll> sol(n), mxs(n);
+    mxs.back() = 0ll;
+    rep(i,n-2ll,0ll,-1ll)
+        mxs[i] = max(mxs[i+1],a[i+1]);
+    rep(i,n) {
+        ll cur = c + (i?psum[i-1ll]:0ll);
+        if (i == mx) {
+            sol[i] = 0;
+            continue;
+        }
+        if (a[i] + cur >= mxs[i]) sol[i] = i;
+        else sol[i] = i+1ll;
+    }
+    print_vec(sol);
+}
