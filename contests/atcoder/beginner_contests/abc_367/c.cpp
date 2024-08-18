9dbb118b6d4a27bf36480cb9bc9a6839d8a3a937
vrangr1 <anavprasadanav@gmail.com>
2024-08-18 17:23:21 +0530

    atcoder abc 367: solved 5/7

---
+++ contests/atcoder/beginner_contests/abc_367/c.cpp
@@ -0,0 +1,70 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Sat Aug 17 17:35:14 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST1;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> r(n);
+    rep(i,n) cin >> r[i];
+    auto rec = [&](auto &&self, int i, int s, vector<int> &v) -> void {
+        if (i == n) {
+            if (s%k == 0) print_vec(v);
+            return;
+        }
+        rep(c,1,r[i],1) {
+            v.push_back(c);
+            self(self,i+1,s+c,v);
+            v.pop_back();
+        }
+    };
+    vector<int> v;
+    rec(rec,0,0,v);
+}
---
+++ contests/atcoder/beginner_contests/abc_367/e.cpp
@@ -0,0 +1,89 @@
+/***************************************************
+* Author  : Anav Prasad
+* Nick    : vrangr
+* Created : Sat Aug 17 18:11:42 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+#define yes "YES\n"
+#define no "NO\n"
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+// IMPORT SNIPPETS HERE
+
+// END OF SNIPPETS
+
+int main() {
+	fastIO;
+	TEST1;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    ll n, k; cin >> n >> k;
+    vector<ll> x(n), a(n);
+    rep(i,n) {
+        cin >> x[i];
+        --x[i];
+    }
+    rep(i,n) cin >> a[i];
+    if (k == 0ll) {
+        print_vec(a);
+        return;
+    }
+    ll m = 64ll-__builtin_clzll(k);
+    vector<vector<ll>> jump(n,vector<ll>(m));
+    rep(j,m) {
+        rep(i,n) {
+            if (!j) {
+                jump[i][j] = x[i];
+                continue;
+            }
+            jump[i][j] = jump[jump[i][j-1]][j-1];
+        }
+    }
+    vector<ll> p(n);
+    rep(i,n) {
+        ll t = k, v = i;
+        while(t) {
+            ll c = 63ll-__builtin_clzll(t);
+            assert(c < m);
+            v = jump[v][c];
+            t-=(1ll<<c);
+        }
+        p[i] = v;
+    }
+    rep(i,n) cout << a[p[i]] << " \n"[i==n-1];
+
+}
