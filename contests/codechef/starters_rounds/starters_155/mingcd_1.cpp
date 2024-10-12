9ca21b54b2bb024a145688cefd13091a671ec4b7
vrangr1 <anavprasadanav@gmail.com>
2024-10-12 17:29:09 +0530

    codechef starters 155 practice: solved mingcd_1, strfirstchar

---
+++ contests/codechef/starters_rounds/starters_155/mingcd_1.cpp
@@ -0,0 +1,58 @@
+/***************************************************
+* Author  : Anav Prasad
+* Handle  : vrangr
+* Created : Fri Oct 11 19:18:31 IST 2024
+****************************************************/
+#include <bits/stdc++.h>
+
+#define debug(...) (void)42
+#ifdef LOCAL
+    #undef debug
+    #include <algo/debug.hpp>
+    const bool DEBUG = true;
+#else
+    [[maybe_unused]] const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef __int128_t i128;
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(v) ((int)(v).size())
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, forsn, debug, forn)(__VA_ARGS__)
+#define forn(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define forsn(i, st, end, d) for(__typeof(end) i = st; (d>0?i<=end:i>=end); i+=((__typeof(end))d))
+template<typename type>inline void print_vec(const vector<type> &v){rep(i,sz(v))cout<<v[i]<<" \n"[i==sz(v)-1];}
+void solve();
+
+int main() {
+	fastIO;
+	TEST;
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << (double)clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<vector<int>> sol(n,vector<int>(m));
+    rep(i,n)
+        rep(j,m) {
+            if (i == j) sol[i][j] = 3;
+            else sol[i][j] = 2;
+        }
+    if (n > m) rep(i,m,n-1,1) sol[i][0] = 3;
+    else if (n < m) rep(j,n,m-1,1) sol[0][j] = 3;
+    rep(i,n) print_vec(sol[i]);
+}
